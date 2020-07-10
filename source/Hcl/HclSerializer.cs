bd082a8729b68ba26ad9619f38285a826662e165
Robert Wagner <robert@wagner.id.au>
2020-07-10 12:45:16 +1000

    Implemented the HclWriter

---
+++ source/Hcl/HclSerializer.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Octopus.Hcl
+{
+    /// <summary>
+    /// This class is thread safe
+    /// </summary>
+    public class HclSerializer
+    {
+        public HclSerializer(HclSerializerOptions? options = null)
+        {
+            Options = options ?? new HclSerializerOptions();
+        }
+
+        public HclSerializerOptions Options { get; }
+
+        public string Serialize(HDocument document)
+            => HclConvert.Serialize(document, Options);
+    }
+}
