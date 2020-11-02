e4891aa90323166157bd0a8a37d564c001c2b065
Robert Wagner <robert@wagner.id.au>
2020-11-02 22:23:20 +1000

    Attribute values are no longer printed if they are the default value (except for empty string)

--- source/Ocl/Converters/DefaultAttributeOclConverter.cs
+++ source/Ocl/Converters/DefaultAttributeOclConverter.cs
@@ -1,13 +1,19 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Octopus.Ocl.Converters
 {
-    public class DefaultAttributeOclConverter : OclConverter
+    public class DefaultAttributeOclConverter : IOclConverter
     {
-        public override bool CanConvert(Type type)
+        public bool CanConvert(Type type)
             => OclAttribute.IsSupportedValueType(type);
 
-        public override object? FromElement(OclConversionContext context, Type type, IOclElement element, object? currentValue)
+        public OclDocument ToDocument(OclConversionContext context, object obj)
+            => throw new NotSupportedException("This type does not support conversion to the OCL root document");
+
+        public object? FromElement(OclConversionContext context, Type type, IOclElement element, object? currentValue)
         {
             if (element is OclAttribute attribute)
                 return attribute.Value;
@@ -15,7 +21,23 @@ namespace Octopus.Ocl.Converters
             throw new OclException("Can only convert attribute elements");
         }
 
-        protected override IOclElement ConvertInternal(OclConversionContext context, string name, object obj)
-            => new OclAttribute(GetName(context, name, obj), obj);
+        public IEnumerable<IOclElement> ToElements(OclConversionContext context, string name, object obj)
+        {
+            var type = obj.GetType();
+
+            if (!type.IsValueType && type == null)
+                yield break;
+
+            if (type.IsValueType)
+                if (obj.Equals(false) || obj.Equals(0) || obj.Equals(0m) || obj.Equals(0f) || obj.Equals('\0'))
+                    yield break;
+
+            if(!(obj is string) && obj is IEnumerable enumerable)
+                if(!enumerable.GetEnumerator().MoveNext())
+                    yield break;
+
+            yield return new OclAttribute(context.Namer.FormatName(name), obj);
+        }
+
     }
 }
