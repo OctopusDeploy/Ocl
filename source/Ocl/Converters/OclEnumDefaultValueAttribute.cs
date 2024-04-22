395af2f263bbcbadea4f0c3675116e8392a487f9
Steven Cleve <107827476+stevencl840@users.noreply.github.com>
2024-04-22 14:00:56 +1000

    Added OclEnumDefaultValueAttribute and tests

---
+++ source/Ocl/Converters/OclEnumDefaultValueAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Octopus.Ocl.Converters
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class OclDefaultEnumValueAttribute : Attribute
+    {
+        public OclDefaultEnumValueAttribute(object defaultValue)
+        {
+            if (!(defaultValue is Enum))
+            {
+                throw new ArgumentException("defaultValue must be an enum", nameof(defaultValue));
+            }
+
+            DefaultValue = defaultValue;
+        }
+
+        public object DefaultValue { get; }
+    }
+}
